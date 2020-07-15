4ea099759114f9f3d7eec56127fbd9849ae1368d
300069965 <abhishyam.chennayipalem@myntra.com>
2020-07-15 23:30:20 +0530

    order/orderId

--- src/main/java/com/hackerramp/subscription/services/OrderService.java
+++ src/main/java/com/hackerramp/subscription/services/OrderService.java
@@ -2,6 +2,8 @@ package com.hackerramp.subscription.services;
 
 import com.hackerramp.subscription.db.OrdersRepo;
 import com.hackerramp.subscription.db.entities.OrdersEntity;
+import com.hackerramp.subscription.services.beans.OrderDetailsResponse;
+import com.hackerramp.subscription.services.transformers.OrderResponseTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,10 @@ public class OrderService {
 
     public List<OrdersEntity> getAllOrders() {
         return (List<OrdersEntity>) ordersRepo.findAll();
+    }
+
+    public OrderDetailsResponse getOrder(String uidx) {
+        OrdersEntity ordersEntity = ordersRepo.findByUserId(uidx);
+        return OrderResponseTransformer.transformOrderEntityToResponseObject(ordersEntity);
     }
 }
